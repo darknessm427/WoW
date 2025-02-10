7143b59089dca2b2e64f499d6804a5e1c17a071c
Diana <135276904+Diana-Cl@users.noreply.github.com>
2025-02-10 21:28:10 +0330

    Update warpREvil.py

--- warpREvil.py
+++ warpREvil.py
@@ -91,29 +91,35 @@ temp2 ={
 }]
 }
     
+def byte_to_base64(myb):
+    return base64.b64encode(myb).decode('utf-8')
+     
+
 def generate_public_key(key_bytes):
     # Convert the private key bytes to an X25519PrivateKey object
     private_key = X25519PrivateKey.from_private_bytes(key_bytes)
-
+    
     # Perform the scalar multiplication to get the public key
     public_key = private_key.public_key()
-
+    
     # Serialize the public key to bytes
     public_key_bytes = public_key.public_bytes(
-        encoding=serialization.Encoding.Raw, format=serialization.PublicFormat.Raw
-    )
+        encoding=serialization.Encoding.Raw,
+        format=serialization.PublicFormat.Raw
+    )    
     return public_key_bytes
 
 
+
 def generate_private_key():
-    key = os.urandom(32)
+    key = os.urandom(32)    
     # Modify random bytes using algorithm described at:
     # https://cr.yp.to/ecdh.html.
-    key = list(key)  # Convert bytes to list for mutable operations
+    key = list(key) # Convert bytes to list for mutable operations
     key[0] &= 248
     key[31] &= 127
-    key[31] |= 64
-    return bytes(key)  # Convert list back to bytes
+    key[31] |= 64    
+    return bytes(key) # Convert list back to bytes
 
 
 def register_key_on_CF(pub_key):
@@ -121,27 +127,24 @@ def register_key_on_CF(pub_key):
     # url = 'https://api.cloudflareclient.com/v0a2158/reg'
     # url = 'https://api.cloudflareclient.com/v0a3596/reg'
 
-    body = {
-        "key": pub_key,
-        "install_id": "",
-        "fcm_token": "",
-        "warp_enabled": True,
-        "tos": datetime.datetime.now().isoformat()[:-3] + "+07:00",
-        "type": "Android",
-        "model": "PC",
-        "locale": "en_US",
-    }
+    body = {"key": pub_key,
+            "install_id": "",
+            "fcm_token": "",
+            "warp_enabled": True,
+            "tos": datetime.datetime.now().isoformat()[:-3] + "+07:00",
+            "type": "Android",
+            "model": "PC",
+            "locale": "en_US"}
 
     bodyString = json.dumps(body)
 
-    headers = {
-        "Content-Type": "application/json; charset=UTF-8",
-        "Host": "api.cloudflareclient.com",
-        "Connection": "Keep-Alive",
-        "Accept-Encoding": "gzip",
-        "User-Agent": "okhttp/3.12.1",
-        "CF-Client-Version": "a-6.30-3596",
-    }
+    headers = {'Content-Type': 'application/json; charset=UTF-8',
+               'Host': 'api.cloudflareclient.com',
+               'Connection': 'Keep-Alive',
+               'Accept-Encoding': 'gzip',
+               'User-Agent': 'okhttp/3.12.1',
+               "CF-Client-Version": "a-6.30-3596"
+               }
 
     r = requests.post(url, data=bodyString, headers=headers)
     return r
@@ -153,6 +156,7 @@ def bind_keys():
     
     pub_bytes = generate_public_key(priv_bytes)
     pub_string = byte_to_base64(pub_bytes)
+    
     
     result = register_key_on_CF(pub_string)
     
