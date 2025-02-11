d0b57922d1d56e25c5811f6f9239d6cc0327f286
Diana <135276904+Diana-Cl@users.noreply.github.com>
2025-02-11 17:43:27 +0330

    build: add npm config files

--- warpauto.php
+++ warpauto.php
@@ -1,12 +1,12 @@
 <?php
 
-    $getList = file_get_contents('https://raw.githubusercontent.com/darknessm427/WoW/main/warpauto.json?v1.'.time());
+    $getList = file_get_contents('https://raw.githubusercontent.com/darknessm427/WoW/refs/heads/main/warpauto.json?v1.'.time());
     $strings = explode("\n", $getList);
 
     $warp = "//profile-title: base64:44CY4oC0yrfhtYPKs+G1luKAt+OAmfCThILwk4aD\n";
-    $warp .= "//profile-update-interval: 1\n";
-    $warp .= "//subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=0\n";
-    $warp .= "//profile-web-page-url: https://github.com/mansor427\n\n";
+    $warp .= "//profile-update-interval: 3\n";
+    $warp .= "//subscription-userinfo: upload = 805306368000; download = 2576980377600; total = 6012954214400; expire = 1762677732\n";
+    $warp .= "//profile-web-page-url: https://github.com/darknessm427\n\n";
     $warp .= "\n";
     $warp .= "warp://@auto/?ifp=40-80&ifps=50-100&ifpd=2-4&ifpm=m4#🇮🇷𓄂𓆃 &&detour=warp://@auto/?ifp=30-60&ifps=40-80&ifpd=1-3&ifpm=m6#🇩🇪@darkness_427";
     $warp .= "\n";
